0317d7d03359bc27129cb3050621a43904cc585a
Pipas <ismailpipas@gmail.com>
2025-02-24 00:11:31 +0000

    feat: add icons to the Chrome extension manifest

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 import { TabsList } from "@radix-ui/react-tabs";
 import { Tabs, TabsContent, TabsTrigger } from "@/components/ui/tabs";
-import { Button } from "./components/ui/button";
 import { ConfigurationCard } from "./components/configuration-card";
 
 function App() {
   return (
     <div className="w-[500px] h-[600px]">
-      <div className="border-b border-input p-4 font-semibold text-xl">
-        GraphQL Authorization Header
-      </div>
+      <div className="border-b border-input p-4 font-semibold text-xl">GraphQL Authorization Header</div>
       <div className="p-2">
         <Tabs className="w-full" defaultValue="request">
-          <TabsList className="grid w-full grid-cols-2 px-2 py-2 rounded-lg bg-neutral-200 gap-2">
-            <TabsTrigger value="request" className="cursor-pointer" asChild>
-              <Button size={"lg"}>Request</Button>
+          <TabsList className="grid w-full grid-cols-2 p-1 rounded-lg bg-neutral-200 gap-2">
+            <TabsTrigger value="request" className="cursor-pointer">
+              Request
             </TabsTrigger>
-            <TabsTrigger value="users" className="cursor-pointer" asChild>
-              <Button size={"lg"}>History</Button>
+            <TabsTrigger value="users" className="cursor-pointer">
+              Saved
             </TabsTrigger>
           </TabsList>
           <ConfigurationCard />
